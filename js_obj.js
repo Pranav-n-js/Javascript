dcec416a100d4fed401f199d3bb2b0fcd4ccadf7
Pranav R Nayak N <pranav.n@juegostudioz.com>
2021-03-16 17:41:36 +0530

    Javascript loops

--- js_obj.js
+++ js_obj.js
@@ -5,7 +5,41 @@
 var objects={
 "properties":"data in objects are accessed through properties",
 "array":["we can store","even array"],
-5: "properties of this will be convereted/typecasted to string",
+5: "properties like integer will be convereted/typecasted to string",
 }
 console.log("Object data can be accessed using 'objects.property'",objects.array)
 console.log("Or we can also use  'objects['property']'",objects['properties'])
+
+var prop=5
+console.log("We can also use variable as property",objects[prop])
+objects["5"]="Properties is changed also data"
+objects['add']="WE can add new objects like this or using dot"
+delete objects.add //to delete
+console.log(objects.hasOwnProperty("property"))//to check wether the property is present or not
+
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
+console.log(myMusic[0]["formats"][1])
+
+//while loop in js
+
+var myArray = [];
+var i=2;
+while(i>=0){
+  for(j=0;j<=2;j++){
+  myArray.push(i)
+}
+  i--
+
+}console.log(myArray)
