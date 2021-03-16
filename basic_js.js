95f53551dc76e0d9c41604a40e002f4b1fb53fc9
Pranav R Nayak N <pranav.n@juegostudioz.com>
2021-03-16 12:49:19 +0530

    Basic Javascript operations

---
+++ basic_js.js
@@ -0,0 +1,16 @@
+//single line comment
+/* multiline comment
+is used like this*/
+var a ; // used to create a new variable
+a=10; // assigining the value to new variable
+var b=10; //another way of assigining variable
+var thisIsCammelCase = "most Used variable method" // storing string and usage of cammel case
+var d=10.012 // decimal variable
+var add,mul,div,sub,rem; // various operations on interger or decimal variable
+add = 10 + 20
+mul = 25 * 10
+div = 30 / 25
+sub = 36 - 25
+rem = 45 % 10
+add += 10; mul *= 15; div /= 30 ; sub -= 1;
+console.log(add,mul,div,sub);
