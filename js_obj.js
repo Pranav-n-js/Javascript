83f3c0acdc3efdcb477ec94477724180a72721ff
Pranav R Nayak N <pranav.n@juegostudioz.com>
2021-03-16 15:55:44 +0530

    Javascript objects

---
+++ js_obj.js
@@ -0,0 +1,11 @@
+// javascript  Objects
+
+//if else switch and variours operatiors are in basic_js.js
+
+var objects={
+"properties":"data in objects are accessed through properties",
+"array":["we can store","even array"],
+5: "properties of this will be convereted/typecasted to string",
+}
+console.log("Object data can be accessed using 'objects.property'",objects.array)
+console.log("Or we can also use  'objects['property']'",objects['properties'])
