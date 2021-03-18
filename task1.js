a231ac02c05c682cfcc8245cbb66728c4bc97882
Pranav R Nayak N <pranav.n@juegostudioz.com>
2021-03-18 11:18:06 +0530

    Task Done

--- task1.js
+++ task1.js
@@ -1,46 +1,34 @@
-function checkingNestedObject(entries,replaceObject,sourceObject){
-  var replaceObjectKeys=Object.keys(replaceObject)
-  entries.forEach((keyValue) => {
-      console.log(entries);
-  //  console.log(keyValue[1]);
-    if(typeof(keyValue[1])==="object"){
+function checkingNestedObject(replaceObject,sourceObject){
+Object.entries(sourceObject).forEach((sourceObjectEntries) => {
+    if (typeof(sourceObjectEntries[1])==="object"){
 
-      replaceObjectKeys.forEach((keys) => {
-      //  console.log(keyValue);
-        if(sourceObject[keyValue[0]].hasOwnProperty(keys)){
-        //  console.log("su");
-          sourceObject[keyValue[0]][keys]=replaceObject[keys]
+      Object.keys(replaceObject).forEach((replaceObjectKey) => {
+
+        if(sourceObjectEntries[1].hasOwnProperty(replaceObjectKey)){
+
+          sourceObject[sourceObjectEntries[0]][replaceObjectKey]=replaceObject[replaceObjectKey]
         }
       });
-      var checkObject=Object.entries(keyValue[1])
-
-      checkObject.forEach((value) => {
-        if(typeof(value[1])==="object"){
-//  console.log(value);
-        let object= checkingNestedObject(value,replaceObject,keyValue[1][value[0]])
-        return object
-      //  console.log(object)
-        }
-
-      });
-
-
-
+      objectReplaced=checkingNestedObject(replaceObject,sourceObjectEntries[1])
+      sourceObject[sourceObjectEntries[0]]=objectReplaced
     }
-  });
+});
 
-
+return sourceObject
 }
 function mutationFormula(replaceObject,sourceObject){
 
-  var replaceObjectKeys=Object.keys(replaceObject)  //gets the keys of the replaceObject
-  replaceObjectKeys.forEach((keys) => {
-    if(sourceObject.hasOwnProperty(keys)){
-      sourceObject[keys]=replaceObject[keys]
+  Object.keys(replaceObject).forEach((key) => {
+
+    if(sourceObject.hasOwnProperty(key)){
+
+      sourceObject[key]=replaceObject[key]
     }
+
   });
-  var object=checkingNestedObject(Object.entries(sourceObject),replaceObject,sourceObject)
-  return sourceObject
+  sourceObject=checkingNestedObject(replaceObject,sourceObject)
+  console.log(sourceObject);
+ return sourceObject
 }
 sourceObject={ name: "pranav", profession: "programmer", address: { street: "1st Lane", place: "Mangalore",dist:{taluk:"manglore", place: "something"} } }
 var object= mutationFormula({name: "Ganesh",place:"kottara"}, sourceObject)
