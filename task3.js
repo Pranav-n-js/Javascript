8a385dd6a9c593206c0ddf77ed9dc5c148a9bfcb
Pranav R Nayak N <pranav.n@juegostudioz.com>
2021-03-19 13:53:54 +0530

    subdomain task

--- task3.js
+++ task3.js
@@ -4,14 +4,18 @@
 checkIpAddress(265)
 */
 function subDomain(url){
-var check=/\w+?\w+\.\w+\.\w+/g.test(url)
+var check=/\w+?w*\.\w+\.\w+/g.test(url)
 
 if(check){
   var matchedUrl=url.match(/\/\w+\./)
-  console.log("Output: ",matchedUrl[0].match(/[a-zA-z0-9]+/)[0]);
+  if(matchedUrl===null){
+    matchedUrl=url.match(url.match(/^\w*\./))
+
+  }
+  console.log("Output: ",matchedUrl[0].match(/\w+/)[0]);
 }
 else{
   console.log("NO SUBDOMAIN");
 }
 }
-subDomain("https://frontend.juegostudio.com ")
+subDomain("https://apps.juegostudio.com")
